d26dcc72cb62c9f1f3781fb35089482e5c31873b
Darrick Wiebe <dw@xnlogic.com>
2013-03-09 13:55:24 -0500

    I was linking before setting up the scope which caused all sorts of confusing error messagse

--- client/clang/directive/clangRepeat.cljs
+++ client/clang/directive/clangRepeat.cljs
@@ -41,39 +41,39 @@
                      arrayLength (count collection)
                      collection (kv-seq collection)
                      nextOrder (atom (array-map))
-                     cursor (atom iterStartElement)]
+                     cursor (atom iterStartElement)
+                     populate-scope (fn [childScope index value]
+                                      (aset childScope valueIdent value)
+                                      (aset childScope "$value" value)
+                                      (when keyIdent
+                                        (aset childScope "$key" key)
+                                        (aset childScope keyIdent key))
+                                      (aset childScope "$index" index)
+                                      (aset childScope "$first" (zero? index))
+                                      (aset childScope "$last" (= index (dec arrayLength)))
+                                      (aset childScope "$middle"
+                                            (not (or (zero? index)
+                                                     (= index (dec arrayLength)))))
+                                      childScope)]
                  (loop [index 0 [key value] (first collection) collection (next collection)]
-                   (let [childScope
-                         (if-let [last (@lastOrder value)]
-                           (do
-                             (swap! lastOrder dissoc value)
-                             (swap! nextOrder assoc value last)
-                             (when-not (= index (:index last))
-                               (! last :index index)
-                               (.after @cursor (:element last)))
-                             (reset! cursor (:element last))
-                             (:scope last))
-                           (do
-                             (let [childScope (.$new scope)]
-                               (linker childScope
-                                       (fn [clone]
-                                         (.after @cursor clone)
-                                         (reset! cursor clone)
-                                         (swap! nextOrder
-                                                assoc value {:scope childScope
-                                                             :element clone
-                                                             :index index})))
-                               childScope)))]
-                     (aset childScope valueIdent value)
-                     (aset childScope "$value" value)
-                     (when keyIdent
-                       (aset childScope "$key" key)
-                       (aset childScope keyIdent key))
-                     (aset childScope "$index" index)
-                     (aset childScope "$first" (zero? index))
-                     (aset childScope "$last" (= index (dec arrayLength)))
-                     (aset childScope "$middle" (not (or (zero? index)
-                                                         (= index (dec arrayLength))))))
+                   (if-let [last (@lastOrder value)]
+                     (do
+                       (swap! lastOrder dissoc value)
+                       (swap! nextOrder assoc value last)
+                       (when-not (= index (:index last))
+                         (! last :index index)
+                         (.after @cursor (:element last)))
+                       (reset! cursor (:element last))
+                       (populate-scope (:scope last) index value))
+                     (let [childScope (populate-scope (.$new scope) index value)]
+                       (linker childScope
+                               (fn [clone]
+                                 (.after @cursor clone)
+                                 (reset! cursor clone)
+                                 (swap! nextOrder
+                                        assoc value {:scope childScope
+                                                     :element clone
+                                                     :index index})))))
                    (when collection
                      (recur (inc index) (first collection) (next collection))))
                  (doseq [[key {:keys [element scope]}] @lastOrder]
