98bc6d804a3fc6f4f0432f84ba8f0904c7d85de8
Darrick Wiebe <dw@xnlogic.com>
2013-03-09 23:15:59 -0500

    cleanup

--- client/clang/core.cljs
+++
@@ -1,4 +0,0 @@
-(ns clang.core
-  (:require-macros [clang.angular :refer [fnj controller]])
-  (:require clang.sample.todo))
-
--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject
   clang "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "ClojureScript on Angular"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
