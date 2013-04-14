8a2e6497c041ead15cd52a7e3d3e392bae05d0d5
Darrick Wiebe <dw@xnlogic.com>
2013-04-14 15:57:13 -0400

    Using INamed here is not essential. Fixes #8

--- client/clang/js_types.cljs
+++ client/clang/js_types.cljs
@@ -18,19 +18,17 @@
     (keyword? x) (name x)
     :else (str x)))
 
+(defn obj-name [this]
+  (if-let [[_ n] (re-find #"^function (\w+)" (str this))]
+    n
+    "Object"))
+
 ;; Everything in JS inherits from js/Object. If we applied some of these
 ;; methods to random JS classes, we would get very odd results. For
 ;; instance, (count (js/Date.)) is 4
 (defn- obj-only [o method]
   (when-not (identical? (type o) js/Object)
-    (throw (js/TypeError. (str (name (type o)) " does not implement '" (name method) "'")))))
-
-(extend-type function
-  INamed
-  (-name [this]
-    (if-let [[_ n] (re-find #"^function (\w+)" (str this))]
-      n
-      "Object")))
+    (throw (js/TypeError. (str (obj-name (type o)) " does not implement '" (name method) "'")))))
 
 (extend-type object
   ILookup
